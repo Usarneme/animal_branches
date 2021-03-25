f95fa3b1c63d406bb9aa71c2f7e6df05f9385cd9
Usarneme <jamestcoop@gmail.com>
2021-03-25 12:03:40 -0700

    add functionality to the first button (snakes) to hide other elements and show the snake div.
    Co-authored-by: Thomas Russell <t.p.russell19@gmail.com>
    Co-authored-by: Sammai Gutierrez <sammaigtz@hotmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 // USER STORIES:
 
-// The page should ask the user which animal they'd like to learn about.
+// ✅ The page should ask the user which animal they'd like to learn about.
 
-// When a user clicks an animal, display a div containing information about that animal (including pictures).
-
-// If they later select a different animal, make sure to hide this div before displaying information about another animal.
+// When a user clicks an animal, display a div containing information 
+// about that animal (including pictures).
+$(document).ready(function(){
+  $(".snake-button").click(function(){
+    $(".turtles").hide();
+    $(".insects").hide();
+    $(".snakes").show();
+  });
+});
+// If they later select a different animal, make sure to hide this div 
+// before displaying information about another animal.

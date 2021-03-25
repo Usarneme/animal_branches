101c1a0fd39f991302f74d18ac48ae0df68961eb
Usarneme <jamestcoop@gmail.com>
2021-03-25 11:45:16 -0700

    add background images and default hiding.
    Co-authored-by: Thomas Russell <t.p.russell19@gmail.com>
    Co-authored-by: Sammai Gutierrez <sammaigtz@hotmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// USER STORIES:
+
+// The page should ask the user which animal they'd like to learn about.
+
+// When a user clicks an animal, display a div containing information about that animal (including pictures).
+
+// If they later select a different animal, make sure to hide this div before displaying information about another animal.
